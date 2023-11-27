f828c18c083bf0a91c1fdf92b0cbf83c3fca350d
JasperVS <jmvanselm@gmail.com>
2023-11-27 15:34:13 +0100

    Add logging and readmes

---
+++ tracking/logging_utils/logger.py
@@ -0,0 +1,29 @@
+import os
+import logging
+
+# Create a logger
+logger = logging.getLogger(__name__)
+
+# Set the logging level
+logger.setLevel(logging.DEBUG)
+
+# Create a file handler
+file_handler = logging.FileHandler("tracking/logs/logs.txt")
+
+# Create a formatter
+formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s")
+
+# Add the formatter to the file handler
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
+
+# Log some messages
+logger.debug("A debug message")
+
+logger.info("An info message")
+
+logger.warning("A warning message")
+
+logger.error("An error message")
